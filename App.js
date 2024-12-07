2dfce0eadf76b1adb36c7409d508e5745022d9bf
FahadQasim283 <fahadqasim3310@gmail.com>
2024-12-07 17:10:53 +0500

    updated

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { ThemeProvider } from "./lib/apptheme/themecontext";
+import ThemeScreen from "./lib/apptheme/themescreen";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Welcome To React Native Learning</Text>
-    </SafeAreaView>
+    <ThemeProvider>
+        <ThemeScreen />
+    </ThemeProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
-
 export default App;
